9a5d7a0424cd025503ab6afb3e03c744a11fce77
pkrzysiekk <pkrzysiek24@gmail.com>
2024-12-06 15:45:34 +0100

    Add styling and displaying items in cart on cart page

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,14 +1,37 @@
 import NavBar from "./NavBar";
 import BlankCart from "./BlankCart";
+import { useCart } from "./CartProvider";
 
 function CartPage() {
+  const { cart, addToCart } = useCart();
+
+  function handleInputChange() {}
   return (
     <>
       <NavBar />
 
       <main>
         <div className="cart-container">
-          <BlankCart />
+          {cart.length == 0 ? (
+            <BlankCart />
+          ) : (
+            cart.map((item) => (
+              <div className="cart-item" key={item.product.id}>
+                <div className="cart-item-name">
+                  <p>{item.product.title}</p>
+                </div>
+                <div className="buttons">
+                  <button className="minus-button">-</button>
+                  <input
+                    type="number"
+                    onChange={handleInputChange}
+                    value={item.quantity}
+                  />
+                  <button className="plus-button">+</button>
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </main>
     </>
